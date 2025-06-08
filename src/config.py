71731bdc787d03c5b4c4d114e32532104efe1711
Nick <nckrpio@gmail.com>
2025-06-07 18:04:45 -0700

    Added env for usage of secrets in future

--- src/config.py
+++ src/config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") #for OpenAI API usage, if needed
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
